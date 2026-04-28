d19fe62b360beed2264c922318aa1e2286e6ad7a
Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>
2026-04-28 16:28:57 +0200

    feat(wfe): add runtime operations
    Signed-off-by: Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>

--- apps/workflowengine/lib/AppInfo/Application.php
+++ apps/workflowengine/lib/AppInfo/Application.php
@@ -40,7 +40,14 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function boot(IBootContext $context): void {
+		$context->injectFn(Closure::fromCallable([$this, 'emitRuntimeEvent']));
 		$context->injectFn(Closure::fromCallable([$this, 'registerRuleListeners']));
+	}
+
+	private function emitRuntimeEvent(IEventDispatcher $dispatcher, ContainerInterface $container): void {
+		/** @var Manager $manager */
+		$manager = $container->get(Manager::class);
+		$manager->reloadRuntimeOperations();
 	}
 
 	private function registerRuleListeners(IEventDispatcher $dispatcher,
@@ -48,7 +55,10 @@ class Application extends App implements IBootstrap {
 		LoggerInterface $logger): void {
 		/** @var Manager $manager */
 		$manager = $container->get(Manager::class);
-		$configuredEvents = $manager->getAllConfiguredEvents();
+		$configuredEvents = array_merge_recursive(
+			$manager->getAllConfiguredEvents(),
+			$manager->getAllConfiguredRuntimeEvents(),
+		);
 
 		foreach ($configuredEvents as $operationClass => $events) {
 			foreach ($events as $entityClass => $eventNames) {
